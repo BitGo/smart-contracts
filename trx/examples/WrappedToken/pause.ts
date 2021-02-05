3895b56bbb3374458db009ea6b3adad83232c047
ipelaez-simtlix <ipelaez@simtlix.com>
2021-02-05 15:26:24 -0300

    STLX-6 Add Abis, README and examples

--- trx/examples/WrappedToken/pause.ts
+++ trx/examples/WrappedToken/pause.ts
@@ -7,3 +7,10 @@ const contract = getContractsFactory('trx').getContract('WrappedToken').instance
 
 const result = contract.methods().pause.call({});
 console.log(result.data);
+
+const decoder = getContractsFactory('trx').getDecoder();
+
+const wbtcData = result.data.slice(2);
+const decoded = decoder.decode(Buffer.from(wbtcData, 'hex'));
+console.log(`\nTransfer decoded : cbce51775299bc0b2c87bd0724d6c1e72f7d2d3f0f82c68c1ec0155772a5c9b4 \n`, decoded);
+
